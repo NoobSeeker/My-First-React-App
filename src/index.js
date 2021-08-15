7c5a70b1cffe1b509ce8b8beaa516055120597e6
NoobSeeker <I.really.need.cloud@gmail.com>
2021-08-15 17:04:46 +0200

    adding project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import { robots } from './robots'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CardList robots={robots} />
   </React.StrictMode>,
   document.getElementById('root')
 );
