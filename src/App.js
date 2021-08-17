26ddf0c39bd5bcb32e5e3a89bcab3317ad829dfe
NoobSeeker <I.really.need.cloud@gmail.com>
2021-08-17 19:23:51 +0200

    changing loading from file to from site
    loading robots from site with json and making card smaller

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import { robots } from './robots';
 import './App.css';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
@@ -17,17 +16,27 @@ class App extends React.Component {
         this.setState( {searchfield: event.target.value} );
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    }
+
     render() {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.OnSearchChange}/>,
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if(this.state.robots.length === 0) {
+            return (<h1>Loading</h1>)
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.OnSearchChange}/>,
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
